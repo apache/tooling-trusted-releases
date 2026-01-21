48f279bbfed99d520f505e4d0e5841341d5e3bf8
Dave Fisher <dave2wave@comcast.net>
2026-01-21 12:13:11 -0800

    Too large a payload handler

--- atr/server.py
+++ atr/server.py
@@ -700,6 +700,15 @@ def _register_routes(app: base.QuartApp) -> None:
             errorcode = getattr(error, "errorcode")
         return await template.render("error.html", error=str(error), status_code=errorcode), errorcode
 
+    # Add a global error handler for payload too large which will normally be handled in front in httpd server
+    @app.errorhandler(413)
+    async def handle_payload_too_large(error: Exception) -> Any:
+        log.exception(f"Payload_too_large: {error!s}")
+        log.exception("There may follow stack traces from form parsing, please ignore")
+        if quart.request.path.startswith("/api"):
+            return quart.jsonify({"error": "413 Payload Too Large"}), 413
+        return await template.render("error.html", error="413 Payload Too Large", traceback="", status_code=413), 413
+
     # Add a global error handler in case a page does not exist.
     @app.errorhandler(404)
     async def handle_not_found(error: Exception) -> Any:
