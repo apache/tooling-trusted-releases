d694e28d4c31f07cb57dc0f4ab03924a00db2681
Dave Fisher <dave2wave@comcast.net>
2026-03-04 15:40:10 -0800

    Revert "Is this change breaking"
    This reverts commit ad7efe7da221979a41477bdeb63846f4e0ba28d8.

--- atr/mail.py
+++ atr/mail.py
@@ -136,7 +136,7 @@ async def _send_via_relay(from_addr: str, to_addr: str, msg_bytes: bytes) -> Non
     context.minimum_version = ssl.TLSVersion.TLSv1_2
 
     smtp = aiosmtplib.SMTP(
-        hostname=_MAIL_RELAY, port=_SMTP_PORT, timeout=_SMTP_TIMEOUT, tls_context=context
+        hostname=_MAIL_RELAY, port=_SMTP_PORT, timeout=_SMTP_TIMEOUT, tls_context=context, start_tls=True
     )
     await smtp.connect()
     log.info(f"Connected to {smtp.hostname}:{smtp.port}")
