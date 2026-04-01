7a8773993451bdd1ddbafb3e11cf9d66fa23f9b8
Sean B. Palmer <sean@miscoranda.com>
2026-04-01 19:01:25 +0100

    Migrate code from asfpy to the pubsub module

--- atr/pubsub.py
+++ atr/pubsub.py
@@ -16,16 +16,27 @@
 # under the License.
 
 import asyncio
+import json
 import os
 import pathlib
 import urllib.parse
+from collections.abc import AsyncGenerator
 from typing import Any
 
-import asfpy.pubsub
+import aiohttp
 
 import atr.ldap as ldap
 import atr.log as log
 import atr.svn.commits as commits
+
+# The server sends keepalives every 5 seconds, so we should see
+# activity well within this timeout period.
+_DEFAULT_INACTIVITY_TIMEOUT = 11
+### for debug:
+# _DEFAULT_INACTIVITY_TIMEOUT = 4.5
+
+# Default read buffer size. Max payload size in pypubsub is 256kb (plus metadata and JSON overhead)
+_DEFAULT_READ_BUFFER_SIZE = 300 * 1024
 
 
 def is_ldap_payload(payload: dict[str, Any]) -> bool:
@@ -34,6 +45,99 @@ def is_ldap_payload(payload: dict[str, Any]) -> bool:
 
 def is_commit_payload(payload: dict[str, Any]) -> bool:
     return "commit" in payload.get("pubsub_topics", [])
+
+
+#
+# TYPICAL USAGE:
+#
+#   async for payload in listen(PUBSUB_URL):
+#
+# This will produce a series of payloads, forever.
+#
+# NOTE: this listener is intended for pypubsub, which terminates
+#   payloads with a newline. The old svnpubsub used NUL characters,
+#   so this client will not work with that server.
+#
+
+
+async def listen(
+    pubsub_url: str,
+    username: str | None = None,
+    password: str | None = None,
+    sock_read: float | None = None,
+    buffersize: int | None = None,
+) -> AsyncGenerator[Any | None]:
+    if username:
+        if password is None:
+            raise ValueError("PubSub password is required")
+        auth = aiohttp.BasicAuth(username, password)
+    else:
+        auth = None
+
+    if sock_read is None:
+        sock_read = _DEFAULT_INACTIVITY_TIMEOUT
+    ct = aiohttp.ClientTimeout(sock_read=sock_read)
+
+    if buffersize is None:
+        buffersize = _DEFAULT_READ_BUFFER_SIZE
+
+    async with aiohttp.ClientSession(auth=auth, timeout=ct, read_bufsize=buffersize) as session:
+        # Retry immediately, and then back it off.
+        delay = 0.0
+
+        ### tbd: look at event loop, to see if it has been halted
+        while True:
+            log.debug("Opening new connection...")
+            try:
+                async for payload in _process_connection(session, pubsub_url):
+                    if not payload:
+                        pass  ### tbd?: event loop killed or hit EOF
+
+                    # We got a payload, so reset the DELAY.
+                    delay = 0.0
+
+                    yield payload
+
+            except (
+                ConnectionRefusedError,
+                aiohttp.ClientConnectorError,
+                aiohttp.ServerTimeoutError,
+                aiohttp.ClientPayloadError,
+            ) as e:
+                log.error(f"Connection failed ({type(e).__name__}: {e}), reconnecting in {delay} seconds")
+                await asyncio.sleep(delay)
+
+                # Back off on the delay. Step it up from 0s, doubling each
+                # time, and top out at 30s retry. Steps: 0, 2, 6, 14, 30.
+                delay = min(30.0, (delay + 1.0) * 2)
+
+
+async def _process_connection(session, pubsub_url):
+    # Connect to pubsub and listen for payloads.
+    async with session.get(pubsub_url) as conn:
+        # print('LIMITS:', conn.content.get_read_buffer_limits())
+
+        while True:
+            # The pubsub server defines stream payloads as:
+            #    ENCODED_JSON(payload)+"\n"
+            #
+            # Due to the encoding, bare newlines will not occur
+            # within the encoded part. Thus, we can read content
+            # until we find a newline.
+            #
+            # Note: this newline is in RAW, but the json loader
+            # ignores it.
+            try:
+                raw = await conn.content.readuntil(b"\n")
+            except ValueError as e:
+                log.error(f'Saw "{e}"; re-raising as ClientPayloadError to close/reconnect')
+                raise aiohttp.ClientPayloadError("re-raised from ValueError in readuntil()")
+
+            if not raw:
+                # We just hit EOF.
+                yield None
+
+            yield json.loads(raw)
 
 
 class PubSubListener:
@@ -76,7 +180,7 @@ class PubSubListener:
         log.info(f"PubSubListener starting with URL: {full_url}")
 
         try:
-            async for payload in asfpy.pubsub.listen(
+            async for payload in listen(
                 full_url,
                 username=self.username,
                 password=self.password,
