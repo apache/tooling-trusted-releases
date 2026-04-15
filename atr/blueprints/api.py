9ed3ba0197aaa79b05745cea812a293f58f5ae98
Sean B. Palmer <sean@miscoranda.com>
2026-04-15 15:41:29 +0100

    Allow the display of deliberately user facing error messages

--- atr/blueprints/api.py
+++ atr/blueprints/api.py
@@ -151,14 +151,19 @@ def _exempt_blueprint(app: base.QuartApp) -> None:
 @_BLUEPRINT.errorhandler(base.ASFQuartException)
 async def _handle_asfquart_exception(err: base.ASFQuartException) -> tuple[quart.Response, int]:
     status = getattr(err, "errorcode", 500)
-    return _json_error(str(err), status)
+    message = str(err)
+    if (status >= 500) and (not config.is_dev_environment()):
+        log.error("Unhandled exception", exc_info=(type(err), err, err.__traceback__))
+        message = "Internal server error"
+    return _json_error(message, status)
 
 
 @_BLUEPRINT.errorhandler(Exception)
 async def _handle_generic_exception(err: Exception) -> tuple[quart.Response, int]:
+    log.error("Unhandled exception", exc_info=(type(err), err, err.__traceback__))
     if config.is_dev_environment():
         return _json_error(str(err), 500)
-    return _json_error("Exception", 500)
+    return _json_error("Internal server error", 500)
 
 
 @_BLUEPRINT.errorhandler(exceptions.HTTPException)
